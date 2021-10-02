774acb1d67179830019a4d49caeb76c5fef7659d
shiva123-coder <sb4_cbu@yahoo.com>
2021-10-02 14:19:19 +0000

    Added item_info template and added functionality to display indivisual item datails to the user.

--- menu/urls.py
+++ menu/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_menu, name='menu'),
+    path('<item_id>', views.item_info, name='item_info'),
 ]
 
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Item, Category
+
 def all_menu(request):
     """ A view to return the items """
 
@@ -7,3 +10,16 @@ def all_menu(request):
         'items': items,
     }
     return render(request, 'menu/menu.html', context)
+
+
+
+def item_info(request, item_id):
+    """ A view to render individual item details from menu """
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'menu/item_info.html', context)
