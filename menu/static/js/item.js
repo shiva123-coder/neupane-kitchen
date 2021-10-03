00f0a15ce227b56c47c78df2e22987f0dfbcb261
shiva123-coder <sb4_cbu@yahoo.com>
2021-10-03 22:52:20 +0000

    Added javascript function using jQuery to create sorting functionality.

---
+++ menu/static/js/item.js
@@ -0,0 +1,21 @@
+// sort option from the select box
+$('#sort-selection').change(function() {
+    let sort_selector = $(this);
+    let currentUrl = new URL(window.location);
+    let selectedVal = sort_selector.val();
+
+    if(selectedVal != "reset"){
+        let sort = selectedVal.split("_")[0];
+        let direction = selectedVal.split("_")[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+
+        window.location.replace(currentUrl);
+    }
+});
