3ab1ae29e5c0cfa8d3e3c78f4e5699bd8607aa39
shiva123-coder <sb4_cbu@yahoo.com>
2021-10-10 20:25:38 +0000

    Added signals and form.

---
+++ checkout/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+            'full_name',
+            'email',
+            'contact_number',
+            'address',
+            'door_no',
+            'town_or_city',
+            'postcode',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'contact_number': 'Contact Number',
+            'address': 'Street Name',
+            'postcode': 'Post Code',
+            'town_or_city': 'Town or City',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'form-input'
+            self.fields[field].label = False
+
