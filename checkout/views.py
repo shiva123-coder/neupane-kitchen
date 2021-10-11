ba969c5c2b03831ecb0f956f1324a593e53bdc0a
shiva123-coder <sb4_cbu@yahoo.com>
2021-10-11 20:23:09 +0000

    Added stripe element, also updated some bootstrap margin, padding and border classes to HTML inorder to improve user experience.

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JiFwUIpMq2ondKYU9lCh9Y8Fo9WFnii1hIpX2y1N2WzcJAGnTIOg9kGD2MODjOk1PI0apL6VlVlT2hkBaukCOh700Twlnv49Z',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
