7c3fcff3c3db30ab3a02ca0f378d650c865eab4e
shiva123-coder <sb4_cbu@yahoo.com>
2021-10-19 22:46:32 +0000

    Added cache checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +10,29 @@ from menu.models import Item
 from basket.contexts import basket_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    """ view to cache data once checkout form submitted"""
+    try:
+        payment_intent_id = request.POST.get(
+            "client_secret").split("_secret")[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+
+        stripe.PaymentIntent.modify(payment_intent_id, metadata={
+            "username": request.user,
+            "save_info": request.POST.get("save_info"),
+            "basket": json.dumps(request.session.get("basket", {}))
+        })
+
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, "Sorry! your payment \
+            can't be proceeed right now. Please try again later.")
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -16,7 +41,6 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     
-
     if request.method == 'POST':
         basket = request.session.get('basket', {})
 
@@ -125,3 +149,5 @@ def checkout_success(request, order_number):
     }
 
     return render(request, template, context)
+
+
