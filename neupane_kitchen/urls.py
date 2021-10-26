263e09524b6e3d74fe4223e738c04f2b57c56b08
shiva123-coder <sb4_cbu@yahoo.com>
2021-10-26 11:12:08 +0000

    Added profile view, urls and profile template.

--- neupane_kitchen/urls.py
+++ neupane_kitchen/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('menu/', include('menu.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """
+    Display user's profile.
+    """
+    template = 'profiles/profile.html'
+    context = {
+
+    }
+
+    return render(request, template, context)
