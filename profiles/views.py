a14d64b60a0752ddcac15f7f3ee4def849e152f6
shiva123-coder <sb4_cbu@yahoo.com>
2021-10-28 22:30:37 +0000

    Added view to diplay order history and also updated its url.

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
+from django.contrib import messages
+
+from checkout.models import Order
 
 
 def profile(request):
@@ -7,9 +11,48 @@ def profile(request):
     Display user's profile.
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/user_profile.html'
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             "Thank you ! Your profile has been updated successfully")
+        else:
+            messages.error(request,
+                           "Sorry! your details can't be save now,\
+                            please re-check the input and try again")
+
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
+
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        "on_profile_page": True,
+    }
+        
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """
+    display the users previous order
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, "This is your order from past")
+
+    template = "checkout/checkout_success.html"
+    context = {
+        "order": order,
+        "past_order": True,
     }
 
-    return render(request, template, context)
+    return render(request,
+                  template,
+                  context)
