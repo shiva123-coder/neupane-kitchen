64e0e50f8e0f37c8fcc2d693545ab4be1b77558b
shiva123-coder <sb4_cbu@yahoo.com>
2021-10-31 13:12:38 +0000

    Updated database setting on settings.py to conncet with postgres, installed requirements for heroku deployment and started deployment process.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn neupane_kitchen.wsgi:application
--- neupane_kitchen/settings.py
+++ neupane_kitchen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-w*prlcwpuej%@wr(bijag&15l#w(84hxr#7wt@8^yx7yuiyki0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['neupane-kitchen.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -107,12 +108,17 @@ WSGI_APPLICATION = 'neupane_kitchen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
