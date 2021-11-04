1ca46b576c9de5ac4b7cc0a356083fd8e96b5e3a
shiva123-coder <sb4_cbu@yahoo.com>
2021-11-04 15:18:15 +0000

    Updated reviews view and also added reviews template.

--- reviews/views.py
+++ reviews/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .forms import ReviewForm
 from .models import Review
 
 
 def food_reviews(request):
     """show all reviews"""
-
-    form = ReviewForm(request.POST)
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.reviewing_user = request.user
+            review.rating = int(request.POST.get('rating'))
+            review.save()
+            
     reviews = Review.objects.all()
+    p = Paginator(reviews, 8)
+    page_number = request.GET.get('page')
+    page_obj = p.get_page(page_number)
 
+    form = ReviewForm()
     template = "reviews/food_reviews.html"
 
     context = {
-        "form": form,
+        'form': form,
+        'page_obj': page_obj,
     }
 
     return render(request,
