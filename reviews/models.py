4d5052f853f010d478032c6cabc589563851489c
shiva123-coder <sb4_cbu@yahoo.com>
2021-11-08 17:44:43 +0000

    Updated fields.

--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+
+from profiles.models import UserProfile
+from menu.models import Item
 
 
 class Review(models.Model):
-    reviewing_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    review = models.TextField(null=False, blank=False)
-    rating = models.IntegerField(null=False, blank=False)
+    RATE = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+    
+    reviewer = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL, null=True)
+    item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(null=False, blank=True, max_length=50)
+    comment = models.TextField(null=False, blank=False)
+    rating = models.IntegerField(choices=RATE)
+    date = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
-        return self.review
+        return f'{self.reviewer}'
