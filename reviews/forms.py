8558b2d56b4de686c0718a129129d164a665d106
shiva123-coder <sb4_cbu@yahoo.com>
2021-11-08 17:45:53 +0000

    Updated fields to match with fields on the model.

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,10 +3,31 @@ from .models import Review
 
 
 class ReviewForm(forms.ModelForm):
-
     class Meta:
         model = Review
-        fields = (
-            "review",
-            "rating"
-        )
+        exclude = (
+            'reviewer',
+            'date',
+            'item'
+            )
+
+        fields = ['title', 'comment', 'rating']
+
+        labels = {
+            'rating': 'Rating',
+        }
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Title',
+            'comment': 'comment',
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'rating':
+                placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].label = False
