95719267728cb81b355939f86a33a6b33e36dd7b
shiva123-coder <sb4_cbu@yahoo.com>
2021-11-09 21:48:21 +0000

    Added JS to disable the + and - button to restrict the user input quantity.

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,29 @@
 
 // + and - sign to increase and decrease the quantity once clicked
-// + buttton to increase
+// + buttton to increase the quantity
 $('.increase-qty').click(function(e) {
     e.preventDefault();
     var newInput = $(this).closest('.input-group').find('.qty_input');
     var newValue = parseInt($(newInput).val());
-    $(newInput).val(newValue + 1);
+// + button disable once input value reach 50
+    if( newValue > 49 ){
+        $('increase-qty').disabled = true;
+     }
+     else {
+        $(newInput).val(newValue + 1);
+     }
 });
 
-// - button to decrease
+// - button to decrease the input quantity
 $('.decrease-qty').click(function(e) {
     e.preventDefault();
     var newInput = $(this).closest('.input-group').find('.qty_input');
     var newValue = parseInt($(newInput).val());
-    $(newInput).val(newValue - 1);
+// + button disable to restric user from iputting the quantity less than 1
+    if( newValue < 2 ){
+        $('decrease-qty').disabled = true;
+     }
+     else {
+        $(newInput).val(newValue - 1);
+     }
 });
