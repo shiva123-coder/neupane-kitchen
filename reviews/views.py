1e8dcc473edb1f10eb2f3b00fa1cfbb1ff8d38c9
shiva123-coder <sb4_cbu@yahoo.com>
2021-11-10 09:21:40 +0000

    Added if/else to redirect user to login page from review page, if user is not logged in.

--- reviews/views.py
+++ reviews/views.py
@@ -11,34 +11,29 @@ from profiles.models import UserProfile
 def add_review(request, item_id):
     """view to add reviews"""
     item = get_object_or_404(Item, pk=item_id)
-    user = get_object_or_404(UserProfile, user=request.user)
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            title = form.cleaned_data['title']
-            comment = form.cleaned_data['comment']
-            rating = form.cleaned_data['rating']
-            Review.objects.create(
-                reviewer=user,
-                item=get_object_or_404(Item, pk=item_id),
-                title=title,
-                rating=rating,
-                comment=comment)
-            messages.success(request, 'Revied added.')
-            return redirect(reverse('item_info', args=[item_id]))
-        else:
-            messages.error(request, 'Sorry! we are unable to add your review. \
-                    Please check your input and try again')
+  
+    if request.user.is_authenticated:
+        user = get_object_or_404(UserProfile, user=request.user)
+        if request.method == 'POST':
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                title = form.cleaned_data['title']
+                comment = form.cleaned_data['comment']
+                rating = form.cleaned_data['rating']
+                Review.objects.create(
+                    reviewer=user,
+                    item=get_object_or_404(Item, pk=item_id),
+                    title=title,
+                    rating=rating,
+                    comment=comment)
+                messages.success(request, 'Reviewd added.')
+                return redirect(reverse('item_info', args=[item_id]))
+            else:
+                messages.error(request, 'Sorry! we are unable to add your review. \
+                        Please check your input and try again')
     else:
-        form = ReviewForm()
-    template = 'reviews/add_review.html'
-    context = {
-        'form': form,
-        'item': item,
-        'on_review_page': True,
-    }
-
-    return render(request, template, context)
+        messages.info(request, 'You must be signed in to add review.')
+        return redirect(reverse('account_login'))
 
 
 def edit_review(request, review_id):
