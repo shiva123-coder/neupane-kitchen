e6264a52883cdbaccaa9bfcbf4839316ca90ed5a
shiva123-coder <sb4_cbu@yahoo.com>
2021-11-14 14:02:00 +0000

    removed blogger field as this field no longer required.

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(null=True, blank=True)
-    blogger = models.ForeignKey(User, on_delete=models.CASCADE)
+    # blogger = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
 
