f50d3bdc58383b4c771b91803604d803bf60edca
shiva123-coder <sb4_cbu@yahoo.com>
2021-11-15 05:13:48 +0000

    Refactored the codes in basket template to make the page responsive, also added some styles.

--- neupane_kitchen/settings.py
+++ neupane_kitchen/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if "IN_DEVELOPMENT" in os.environ:
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['neupane-kitchen.herokuapp.com', 'localhost']
 
@@ -115,17 +112,17 @@ WSGI_APPLICATION = 'neupane_kitchen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
+    }
 
 
 # Password validation
