ff169a6be84ca54e5dee088b7ae206fc63d25a98
shiva123-coder <sb4_cbu@yahoo.com>
2021-11-16 18:54:51 +0000

    Moved code for scroll icon to base template, added bootstrap classes to improve user experience.

--- static/js/main.js
+++ static/js/main.js
@@ -27,3 +27,19 @@ $('.decrease-qty').click(function(e) {
         $(newInput).val(newValue - 1);
     }
 });
+
+//scroll icon
+// scroll icon only visible once screen is scroll down to over 100px
+$(window).scroll(function(){
+    var screenHeight = $(window).scrollTop();
+    if (screenHeight > 100) {
+        $('#scroll-icon').fadeIn();
+    } else {
+        $('#scroll-icon').fadeOut(2000);
+    }
+});
+
+// once click on scroll icon, page scroll back to the top
+$('#scroll-icon').click(function(e){
+    window.scrollTo(0,0)
+});
