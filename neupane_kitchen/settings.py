af62be11c1a2c096e8cd6b9f7489ff2266dd7ec7
shiva <sb4_cbu@yahoo.com>
2021-11-22 14:22:58 +0000

    Initial commit.

--- neupane_kitchen/settings.py
+++ neupane_kitchen/settings.py
@@ -9,9 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 from pathlib import Path
 import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
