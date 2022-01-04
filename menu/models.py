6cefc506d180cb1647bc750d41dd3624f69b2b57
shiva123-coder <sb4_cbu@yahoo.com>
2022-01-04 16:07:47 +0000

    Addedallergen field to model

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 
 class Category(models.Model):
@@ -21,12 +22,33 @@ class Category(models.Model):
         return self.display_name
 
 
+ALLERGEN_CHOICES = (
+        ('Celery', 'Celery'),
+        ('Cereals containing gluten', 'Cereals containing gluten'),
+        ('Crustaceans', 'Crustaceans'),
+        ('Egg', 'Egg'),
+        ('Fish', 'Fish'),
+        ('Lupin', 'Lupin'),
+        ('Milk', 'Milk'),
+        ('Molluscs', 'Molluscs'),
+        ('Mustard', 'Mustard'),
+        ('Tree nuts/Nuts', 'Tree nuts/Nuts'),
+        ('Peanuts', 'Peanuts'),
+        ('Sesame Seeds', 'Sesame Seeds'),
+        ('Soya', 'Soya'),
+        ('Sulphur Dioxide/Sulphite', 'Sulphur Dioxide/Sulphite')   
+    )
+
+
 class Item(models.Model):
     category = models.ForeignKey('Category', null=True,
                                  blank=True, on_delete=models.SET_NULL)
-    tag = models.CharField(max_length=170, null=True, blank=True)
     name = models.CharField(max_length=170)
-    description = models.TextField()
+    heading = models.TextField(max_length=150, null=True, blank=True)
+    description = models.TextField(max_length=270, null=True, blank=True)
+    allergen = MultiSelectField(
+        choices=ALLERGEN_CHOICES, null=True, blank=False)
+    note = models.TextField(max_length=80, null=True, blank=True)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
