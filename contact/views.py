c6ab6e67f77dba27544c45c6e0f4e32790109ed0
shiva123-coder <sb4_cbu@yahoo.com>
2022-01-07 18:05:27 +0000

    Updated message functionality on contact us page.

--- contact/views.py
+++ contact/views.py
@@ -1,27 +1,31 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse)
 from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
+from django.contrib import messages
+from django.conf import settings
 
 
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = "Website Inquiry"
+            subject = 'Enquiry'
             body = {
                 'full_name': form.cleaned_data['full_name'],
                 'email': form.cleaned_data['email'],
                 'contact_number': form.cleaned_data['contact_number'],
                 'message': form.cleaned_data['message'],
                 }
-            message = "\n".join(body.values())
+            message = '\n'.join(body.values())
+            form.save()
 
         try:
             send_mail(
                 subject, message, 'admin@example.com', ['admin@example.com'])
+            return render(request, 'contact/contact_success.html')
         except BadHeaderError:
             return HttpResponse('Invalid header found.')
-            return redirect("home")
+           
     form = ContactForm()
-    return render(request, "contact/contact.html", {'form': form})
+    return render(request, 'contact/contact.html', {'form': form})
