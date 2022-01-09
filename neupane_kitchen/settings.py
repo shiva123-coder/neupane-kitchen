76703f40fe2ee0e91c4ebb6b502f99f350b2bb7f
shiva123-coder <sb4_cbu@yahoo.com>
2022-01-09 13:41:58 +0000

    heroku deploy trigger

--- neupane_kitchen/settings.py
+++ neupane_kitchen/settings.py
@@ -123,17 +123,17 @@ WSGI_APPLICATION = 'neupane_kitchen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/databases
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
 
 
 # Password validation
@@ -206,7 +206,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
-    DEFAULT_FROM_EMAIL = 'neupanekitchen@onlinefood.com'
+    DEFAULT_FROM_EMAIL = 'neupanekitchen.co.uk'
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
