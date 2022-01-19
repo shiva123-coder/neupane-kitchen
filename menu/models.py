d93ff945a50f2d09759784e0e9b04dc8fa4a979f
shiva123-coder <sb4_cbu@yahoo.com>
2022-01-19 19:56:10 +0000

    Updated model field in the menu app.

--- menu/models.py
+++ menu/models.py
@@ -45,12 +45,12 @@ class Item(models.Model):
     category = models.ForeignKey('Category', null=True,
                                  blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=170)
-    heading = models.TextField(max_length=150, null=True, blank=True)
-    description = models.TextField(max_length=270, null=True, blank=True)
-    ingredient = models.TextField(max_length=370, null=True, blank=True)
+    heading = models.TextField(max_length=250, null=True, blank=True)
+    description = models.TextField(max_length=370, null=True, blank=True)
+    ingredient = models.TextField(max_length=700, null=True, blank=True)
     allergen = MultiSelectField(
         choices=ALLERGEN_CHOICES, null=True, blank=False)
-    note = models.TextField(max_length=80, null=True, blank=True)
+    note = models.TextField(max_length=280, null=True, blank=True)
     price = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
     image = models.ImageField(null=True, blank=True)
 
