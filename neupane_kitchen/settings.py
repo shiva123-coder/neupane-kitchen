dd06d2d7b63e28c699c2c1397e85a8a943a62759
shiva123-coder <sb4_cbu@yahoo.com>
2022-01-24 21:53:33 +0000

    Fixed setting error

--- neupane_kitchen/settings.py
+++ neupane_kitchen/settings.py
@@ -133,8 +133,8 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
     }
+}
 
 
 # Password validation
